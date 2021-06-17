ac209c3de4f93e4c5601a290ec2c76fe597743e8
geniuser <771282518@qq.com>
2021-06-17 20:08:07 +0800

    2021年6月17号 学习了js模块化的commonjs模块化规范的node实现和browserify实现

---
+++ 26.JS模块化modular/myContent/work(codingByMyself)/02_CommonJS-Node(参考教程2)/modules/module3.js
@@ -0,0 +1,12 @@
+// 使用export.xxx = value 暴露模块
+exports.foo = function () {
+    console.log('foo() module3')
+}
+
+exports.bar = function () {
+    console.log('bar() module3')
+}
+
+exports.arr = [2, 5, 6, 6, 5, 4, 8, 9];
+
+// 不能在使用module.exports = {}进行暴露，会覆盖之前的
